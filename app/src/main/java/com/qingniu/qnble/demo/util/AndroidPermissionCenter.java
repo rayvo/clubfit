c8703b0e3746348a04554c0901a8ef77091b6645
hekang <hekang@yolanda.hk>
2018-08-10 11:44:41 +0800

    发布v0.3.2

--- app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
+++ app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
@@ -26,19 +26,12 @@ public class AndroidPermissionCenter {
     //API>=23时才需要判断权限，扫描时再判断权限
     public static void verifyPermissions(Activity activity) {
         //判断是否已经赋予权限
-        if (ContextCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
-
-            } else {
-                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
-                ActivityCompat.requestPermissions(activity,
-                        PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
-            }
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}
+                    , REQUEST_EXTERNAL_STORAGE);
         }
     }
 
